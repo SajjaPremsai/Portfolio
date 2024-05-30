8f75a1850b545abb20a1da32908fe77b1c7bd567
SajjaPremsai <sajjapremsai8938@gmail.com>
2024-05-30 14:47:09 +0530

    Updated the Portoflio 30th may 2024

--- src/Components/Certifications/data.js
+++ src/Components/Certifications/data.js
@@ -1,119 +1,31 @@
-import cloud from "../../assets/Certificates/Cloud Computing_page-0001.jpg"
-import html from "../../assets/Certificates/Introduction to HTML,CSS & JS_page-0001.jpg"
-import git from "../../assets/Certificates/Git and GitHub_page-0001.jpg"
-import react from "../../assets/Certificates/Developing Front-end using React_page-0001.jpg"
-import node from "../../assets/Certificates/Developing back-end with node.js and express_page-0001.jpg"
-import ds from "../../assets/Certificates/Python for Data Science, AI & Development_page-0001.jpg"
-import ai from "../../assets/Certificates/Python Project for AI & Application Development_page-0001.jpg"
-import django from "../../assets/Certificates/Developing Applications with SQL, Databases, and Django_page-0001.jpg"
-import kuber from "../../assets/Certificates/Introduction to Containers Docker , Kubernetes and Open Shift_page-0001.jpg"
-import faas from "../../assets/Certificates/Application Development using Microservices and Serverless_page-0001.jpg"
-
+import FullStack from "../../assets/Certificates/Full Stack Professional.jpg"
+import java from "../../assets/Certificates/NPTEL/Programming In Java.jpg"
+import tensorflow from "../../assets/Certificates/Tensorflow Developer Certificate.jpg"
 const timelineElements = [
     {
         id: 1,
-        link: "https://www.coursera.org/account/accomplishments/certificate/R6PWBXM587VS",
-        image: cloud,
-        title: "Introduction to Cloud Computing",
-        company: "Coursera-Issued on  11th-March-2023",
-        tags: [
-            "cloud","Iaas","Pass","Saas"
-        ]
+        image: tensorflow, // Ensure this path is correct
+        title: "TensorFlow Developer Certificate",
+        company: "Google",
+        issuedOn: "29-May-2024",
+        tags: ["TensorFlow", "Neural Networks", "RNN", "CNN", "Word Embeddings", "Time Series",]
     },
     {
         id: 2,
-        link: "https://www.coursera.org/account/accomplishments/certificate/PYPQBGAYVSAC",
-        image: html,
-        title: "Introduction to Web Development with HTML,CSS,JavaScript",
-        company: "Coursera-Issued on  30th-March-2023",
-        date: "March 2016 - March 2019",
-        tags: [
-            "HTML", "CSS", "JavaScript"
-        ]
+        image: FullStack, // Ensure this path is correct
+        title: "IBM Full Stack Software Developer",
+        company: "Coursera",
+        issuedOn: "01-Jan-2024",
+        tags: ["ReactJs", "Nodejs", "Express" ,"Containers", "Cloud-Computing","Docker",]
     },
     {
         id: 3,
-        link: "https://www.coursera.org/account/accomplishments/certificate/P4728P25UB88",
-        image: git,
-        title: "Getting Started with Git and GitHub",
-        company: "Coursera-Issued on  7th-April-2023",
-        tags: [
-            "Git", "GitHub", "Version Control"
-        ]
-    },
-    {
-        id: 4,
-        link: "https://www.coursera.org/account/accomplishments/certificate/HS6YMZZSRBAP",
-        image: react,
-        title: "Developing Front-End Apps With React",
-        company: "Coursera-Issued on  11th-May-2023",
-        tags: [
-            "React", "hooks", "components"
-        ]
-    },
-    {
-        id: 5,
-        link: "https://www.coursera.org/account/accomplishments/certificate/GGHQK9U9N3GY",
-        image: node,
-        title: "Developing Back-End Apps with Node.js and Express",
-        company: "Coursera-Issued on  18th-June-2023",
-        tags: [
-            "express", "node", "jwt", "backend"
-        ]
-    },
-    {
-        id: 6,
-        link: "https://www.coursera.org/account/accomplishments/certificate/ZH3G5CNW8R3B",
-        image: ds,
-        title: "Python for Data Science , AI & Development",
-        company: "Coursera-Issued on  27th-June-2023",
-        tags: [
-            "Pandas", "Numpy", "WebScrapping"
-        ]
-    },
-    {
-        id: 7,
-        link: "https://www.coursera.org/account/accomplishments/certificate/2HKPUVTV8HNQ",
-        image: ai,
-        title: "Developing AI Applications with Python and Flask",
-        company: "Coursera-Issued on  30th-June-2023",
-        tags: [
-             "Flask", "WatsonAI"
-        ]
-    },
-    
-    {
-        id: 8,
-        link: "https://www.coursera.org/account/accomplishments/certificate/LGE29K4XJXT7",
-        image: django,
-        title: "Developing Applications with SQL,Databases and Django",
-        company: "Coursera-Issued on  13th-July-2023",
-        tags: [
-            "ORM", "Django", "Python", "MVT"
-        ]
-    },
-    {
-        id: 9,
-        link: "https://www.coursera.org/account/accomplishments/certificate/PDP43CPSWLFV",
-        image: kuber,
-        title: "Introduction to Containers Docker , Kubernetes and Open Shift",
-        company: "Coursera-Issued on  23rd-July-2023",
-        tags: [
-            "containers", "Docker", "Kubectl"
-        ]
-    },
-    
-    {
-        id: 10,
-        link: "https://www.coursera.org/account/accomplishments/certificate/DDUGMAWECZRB",
-        image: faas,
-        title: "Application Development using Microservices and Severless",
-        company: "Coursera-Issued on  6th-August-2023",
-        tags: [
-            "Faas", "ibm-cloud-engine"
-        ]
-    },
-
+        image: java, // Ensure this path is correct
+        title: "Programming in Java",
+        company: "NPTEL",
+        issuedOn: "22-May-2023",
+        tags: ["Core Java","OOPS","Interfaces","Classes & Objects"]
+    }
 ];
 
 export default timelineElements;
--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import "./Skills.css"
-import java from "../../assets/Images/java.png"
-import python from "../../assets/Images/python.png"
+import java from "../../assets/Skills/java.svg"
+import python from "../../assets/Skills/python.svg"
 import Card from './Card.js/Card'
-import git from "../../assets/Images/git.png"
-import Node from "../../assets/Images/node.png"
-import Html from "../../assets/Images/html.png"
-import Docker from "../../assets/Images/docker.png"
-import Kuber from "../../assets/Images/kuber.png"
-import react from "../../assets/Images/React.png"
-import css from "../../assets/Images/css.png"
+import git from "../../assets/Skills/git.svg"
+import Node from "../../assets/Skills/nodejs.svg"
+import Html from "../../assets/Skills/html.svg"
+import Docker from "../../assets/Skills/docker.svg"
+import Kuber from "../../assets/Skills/kubernets.svg"
+import react from "../../assets/Skills/react.svg"
+import css from "../../assets/Skills/css.svg"
+import tensorflow from "../../assets/Skills/tensorflow.svg"
+import numpy from "../../assets/Skills/numpy.svg"
+import pandas from "../../assets/Skills/pandas.svg"
+import matplotlib from "../../assets/Skills/matplotlib.svg"
+import express from "../../assets/Skills/expressjs.svg"
 
 export default function Skills() {
     const data = [
@@ -27,15 +32,17 @@ export default function Skills() {
             "id": 3,
             "title": "Git",
             "path": git
-        }, {
+        },
+        {
             "id": 4,
             "title": "Node",
             "path": Node
-        }, {
+        },
+        {
             "id": 5,
             "title": "Html",
             "path": Html
-        }, 
+        },
         {
             "id": 6,
             "title": "CSS",
@@ -50,12 +57,39 @@ export default function Skills() {
             "id": 8,
             "title": "Docker",
             "path": Docker
-        }, {
+        },
+        {
             "id": 9,
             "title": "Kubernetes",
             "path": Kuber
+        },
+        {
+            "id": 10,
+            "title": "Express",
+            "path": express
+        },
+        {
+            "id": 11,
+            "title": "TensorFlow",
+            "path": tensorflow
+        },
+        {
+            "id": 12,
+            "title": "NumPy",
+            "path": numpy
+        },
+        {
+            "id": 13,
+            "title": "Pandas",
+            "path": pandas
+        },
+        {
+            "id": 14,
+            "title": "Matplotlib",
+            "path": matplotlib
         }
-    ]
+    ];
+
 
 
     return (
